ff4dfc687b99ecbfe464b65723a0d9176c262b44
fanngineer <109322241+fanngineer@users.noreply.github.com>
2023-03-02 11:08:49 +0900

    P1231_중위순회
    배열로 트리 구현, 중위순회

--- Algorithm/Java/SWEA/P1231_중위순회.java
+++ Algorithm/Java/SWEA/P1231_중위순회.java
@@ -1,57 +1,58 @@
 package SWEA;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+import java.util.StringTokenizer;
 
 public class P1231_중위순회 {
-	static String word;
-	public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T = 10;
-        for (int test_case = 1; test_case <= T; test_case++) {
-        	//배열 만들기
-        	int N = sc.nextInt();
-        	sc.nextLine();
-        	String[] keys = new String[N+1];
-        	int[][] tree = new int[N+1][2];
-        	//입력받기
-        	for(int i=1;i<=N;i++) {
-        		String input = sc.nextLine();
-        		String[] info = new String[4];
-        		String[] inputs = input.split(" ");
-        		for(int j=0; j<inputs.length;j++) {
-        			info[j] = inputs[j];
-        		}
-        		int node = Integer.parseInt(info[0]);
-        		String key = info[1];
-        		keys[node] = key;
-        		
-        		if(info[2]==null) {
-        			tree[node][0] = 0;
-        			tree[node][1] = 0;
-        		}else if(info[3]==null){
-        			tree[node][0] = Integer.parseInt(info[2]);
-        			tree[node][1] = 0;
-        		}else {
-        			tree[node][0] = Integer.parseInt(info[2]);
-        			tree[node][1] = Integer.parseInt(info[3].replaceAll("\n", ""));
-        		}
-            }
-        	//순회
-        	word = ""; //반환할 단어
-        	inOrder(1 ,keys, tree);
-            //출력
-            System.out.println("#" + test_case+" "+word);
-        }//for
-    }//main
-	public static void inOrder(int root, String[] keys, int[][] tree) {
-		if(tree[root][0] != 0) {
-			inOrder(tree[root][0], keys, tree);
+	
+	static StringBuilder sb;
+	static int[][] biTree;
+	static String[] datas;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		sb = new StringBuilder();
+		for(int t=1; t<=10;t++) {
+			int N = Integer.parseInt(br.readLine());
+			biTree = new int[N+1][2];
+			datas = new String[N+1];
+			int idx;
+			int leftRight;
+			for(int n=1; n<=N;n++) {
+				st = new StringTokenizer(br.readLine());
+				idx = Integer.parseInt(st.nextToken());
+				datas[idx] = st.nextToken();
+				leftRight = 0;
+				while(st.hasMoreTokens()) {
+					biTree[idx][leftRight] = Integer.parseInt(st.nextToken());
+					leftRight++;
+				}
+			}//for_N 입력
+			//읽기
+			sb.append("#").append(t).append(" ");
+			inOrder(1);
+			bw.write(sb.toString());
+			bw.newLine();
+			sb.setLength(0);
+		}//for_testCase
+		bw.close();
+	}//main
+	static void inOrder(int idx) {
+		int left = biTree[idx][0];
+		int right = biTree[idx][1];
+		if(left != 0) {
+			inOrder(left);
 		}
-		word += keys[root];//읽는 과정
-		if(tree[root][1] != 0) {
-			inOrder(tree[root][1], keys, tree);
+		sb.append(datas[idx]);
+		if(right !=0) {
+			inOrder(right);
 		}
 	}
 }
