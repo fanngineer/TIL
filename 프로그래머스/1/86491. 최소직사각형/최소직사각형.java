a33294b6a3d7eafd7c34ca8892b5425a41d77892
Seonghwan Kim <109322241+fanngineer@users.noreply.github.com>
2024-03-22 19:02:33 +0900

    [level 1] Title: 최소직사각형, Time: 1.32 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,19 +1,12 @@
 import java.util.PriorityQueue;
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
-        PriorityQueue<Integer> pq1 = new PriorityQueue<>((a,b)->(b-a));
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>((a,b)->(b-a));
+        int length = 0;
+        int height = 0;
         for(int[] size : sizes){
-            if(size[1]>size[0]){
-                pq1.add(size[1]);
-                pq2.add(size[0]);
-            }else{
-                pq1.add(size[0]);
-                pq2.add(size[1]);
-            }
+            length = Math.max(length, Math.max(size[1],size[0]));
+            height = Math.max(height, Math.min(size[1],size[0]));
         }
-        answer = pq1.poll() * pq2.poll();
-        return answer;
+        return length * height;
     }
 }
